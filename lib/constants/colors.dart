a48c18d6fc4b5413ec03653b4caa6993b612b448
조현진 <jcn09125@gmail.com>
2025-11-24 19:24:07 +0900

    [FIX] mainGreen 색 변경

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // 연두, 베이지, 흰색
-const Color mainGreen = Color(0xFFFDFFF6);    // 메인 포인트 연두
+const Color mainGreen = (Color(0xFFA9C18D));    // 메인 포인트 연두
 const Color naviGreen= Color(0xFF91BF3F);  // 네비게이션 바 누르면 연두색
 const Color bgBeige = Color(0xFFFBF8F1);      // 베이지 배경
 const Color white = Colors.white;
